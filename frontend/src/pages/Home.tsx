5fc48bef68c127c4b14352d969ab589eb90df374
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-02-09 13:04:54 +0100

    Created Docker image

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,32 @@
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 // import { useNavigate } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 import { getTasks } from "../services/apiService";
+
+const holoLinkClass = `
+    relative
+    px-4 py-2
+    text-green
+    font-semibold
+    rounded-lg
+    transition-all
+    duration-300
+    hover:text-green-400
+    hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.9)]
+    before:absolute
+    before:inset-0
+    before:rounded
+    before:bg-green-400
+    before:opacity-20
+    before:blur-xl
+    before:scale-110
+    before:transition-all
+    before:duration-300
+    hover:before:opacity-50
+    hover:before:scale-80
+    before:pointer-events-none
+  `;
 
 export default function Home() {
     interface DisplayTask {
@@ -58,14 +83,19 @@ export default function Home() {
         </p>
  
       </section>
-      <section>
-        <button
-          onClick={() => {
-            setDisplay();
-          }}
-          className="px-6 py-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 transition-colors duration-300"
-        >Get tasks</button>
-      </section>
+      <Dropdown>
+                <Dropdown.Header>Tasks</Dropdown.Header>
+                    {tasks.map((item, index) => (
+                        <Dropdown.Menu show key={index}>
+                        <Dropdown.Item className={holoLinkClass} onClick={() => {
+                                    // clearToken();
+                                    setDisplay()
+                                    updateDisplay(!display)
+                                }}>{item.name}
+                        </Dropdown.Item>
+                        </Dropdown.Menu>
+                        ))}
+            </Dropdown>
     </Layout>
   );
 }
