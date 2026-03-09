8af42269ef7809bc103d29e6409ca0f28c78d607
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-03-09 13:02:59 +0100

    Update apiService.tsx

--- frontend/src/services/apiService.tsx
+++ frontend/src/services/apiService.tsx
@@ -53,7 +53,7 @@ export const createTask = async (url: string, task: Task) => {
     }
 }
 
-export const deleteTask = async (url: string, id: string) => {
+export const deleteTask = async (url: string, task_id: string) => {
     const token = getToken();
     if (!token) {
         console.warn("No token found. Cannot delete task.");
@@ -67,7 +67,7 @@ export const deleteTask = async (url: string, id: string) => {
                 "Authorization": `Bearer ${token}`,
             },
             body: JSON.stringify({ 
-                id: id
+                id: task_id
             }),
         });
         if (!response.ok) {
